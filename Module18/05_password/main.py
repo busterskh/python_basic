debc0dd23c225c5de9a42040d746a1eaf672124c
Vlad Shevchenko <busterskh@gmail.com>
2022-04-20 08:56:38 +0300

    Fix 5,10 task

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,10 +1,11 @@
 correct = False
 while correct is False:
     password = input('Придумайте пароль: ')
-    for i_pass in password:
-        if i_pass in '1234567890':
-            if password != password.lower():
-                correct = True
+    if len(list(password)) >= 8:
+        for i_pass in password:
+            if i_pass in '1234567890':
+                if password != password.lower():
+                    correct = True
     if correct is True:
         print('Это надёжный пароль!')
     else:
--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,17 +1,35 @@
-def encrypt(message, key):
-    translated = ''
-    for symbol in message:
-        if symbol in LETTERS:
-            num = LETTERS.find(symbol)
-            translated = translated + LETTERS[num - key]
+def shift_word(word, word_shift):
+    word_ln = len(word)
+    shift = word_shift % word_ln
+    return ''.join([word[shift:], word[:shift]])
+
+
+def word_decode(word: str, word_shift):
+    correct_word = shift_word(word, word_shift)
+    return ''.join([chr(ord(symbol) - 1) for symbol in correct_word])
+
+
+def decode_text(txt: str):
+    word_shift = -3
+    for word in txt.split():
+        if word.find('/') != -1:
+            print(word_decode(word, word_shift))
+            word_shift -= 1
         else:
-            translated = translated + symbol
-    return translated
+            print(word_decode(word, word_shift), end=' ')
 
 
-input_text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
-
-LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-encrypt = encrypt(input_text, 1)
-
-print(encrypt)
+txt = (
+    'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm '
+    'yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef '
+    'uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf '
+    'ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf '
+    'Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv '
+    'Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ '
+    'Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs '
+    'uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op '
+    'gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb '
+    'Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ '
+    'bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+)
+decode_text(txt)
