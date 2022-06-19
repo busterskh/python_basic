5c1450ebe7244165e41828230b33e7fe3c254433
Vlad Shevchenko <busterskh@gmail.com>
2022-06-19 17:24:54 +0300

    Done all tasks module28

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+class Open:
+    def __init__(self, name) -> None:
+        self.file_name = name
+        self.open_file = None
+
+    def write(self, *args) -> None:
+        if args:
+            self.open_file.write(str(args))
+        else:
+            text = input('Что запишем в файл?\n')
+            self.open_file.write(text)
+
+    def __enter__(self) -> 'Open':
+        print(f'Открываем файл {self.file_name}')
+        self.open_file = open(self.file_name, 'w')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.open_file.close()
+        print(f'Файл {self.file_name} закрыт.')
+
+
+with Open('test_2.py') as file:
+    file.write()
