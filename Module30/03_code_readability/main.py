c0973fd47bc000e592113eb2d5c4ba253fee0752
Vlad Shevchenko <busterskh@gmail.com>
2022-06-25 11:22:17 +0300

    Done all tasks module30

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from typing import List
+
+
+is_prime: List[int] = list(filter(lambda num:
+                                  True not in [True if num % x == 0 else False for x in range(2, num)],
+                                  [i for i in range(2, 1001)]
+                                  )
+                           )
+print(is_prime)
+
+
+n = 1000
+a = [x for x in range(n + 1)]
+a[1] = 0
+is_prime_2 = []
+
+i = 2
+while i <= n:
+    if a[i] != 0:
+        is_prime_2.append(a[i])
+        for j in range(i, n+1, i):
+            a[j] = 0
+    i += 1
+print(is_prime_2)
--- Module30/04_palindrome_the_return/main.py
+++ Module30/04_palindrome_the_return/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def can_be_poly(val: str):
+    return len(list(filter(lambda x: x % 2, Counter(val).values()))) <= 2
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
