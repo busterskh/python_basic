76e2bfb5645bed786fa96d5a3049a18b8c6dc53c
Vlad Shevchenko <busterskh@gmail.com>
2022-06-29 20:14:16 +0300

    fix 5 task

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -2,5 +2,5 @@ import itertools
 
 numbers = [i for i in range(0, 10)]
 
-for pin in itertools.combinations_with_replacement(numbers, 4):
+for pin in itertools.product(numbers, repeat=4):
     print(pin)
